8af3d68f854bfa7dc6cdc30838ea8f6da479f132
igordiuk17 <igordiuk17@gmail.com>
2025-01-04 23:31:32 +0200

    Commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
+import MyComponents from "./Components/MyComponents.tsx";
+
 
 function App() {
 
 
   return (
     <>
+     Hello Okten
+        {
+            MyComponents({text:'hello2'})
+        }
+        <MyComponents text={'hello1'}/>
 
-    </>
-  )
+          </>
+  );
 }
 
 export default App
---
+++ src/Components/MyComponents.tsx
@@ -0,0 +1,4 @@
+function MyComponent({text}:{text:string}){
+    return <div>   {text}  </div>
+}
+export default MyComponent;
