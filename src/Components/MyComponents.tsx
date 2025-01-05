f81632a7cb2a70bfd7094f31d74b3abcf99e925e
igordiuk17 <igordiuk17@gmail.com>
2025-01-05 19:31:04 +0200

    Commit 2

--- src/Components/MyComponents.tsx
+++ src/Components/MyComponents.tsx
@@ -1,4 +1,5 @@
+
 function MyComponent({text}:{text:string}){
-    return <div>   {text}  </div>
+    return <div className={'text-3xl font-bold underline'}>   {text}  </div>
 }
 export default MyComponent;
